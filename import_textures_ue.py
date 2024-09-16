9f0f6c3bf4460d3abe61a0790b53667ce003de90
yangskin <yangskin@gmail.com>
2024-09-17 02:59:23 +0800

    修正重新导入贴图丢失的关键设置

--- import_textures_ue.py
+++ import_textures_ue.py
@@ -5,17 +5,32 @@ def import_textures():
     EXPORT_TEXTURE_PATH
     ]
 
+    folder_path = "FOLDER_PATH"
+    asset_library:unreal.EditorAssetLibrary = unreal.EditorAssetLibrary()
+
     for path in paths:
         file_name = path[path.rfind("/") + 1 :path.rfind(".")]
+
+        current_texture = asset_library.load_asset(folder_path + "/" + file_name)
+        if current_texture:
+            srgb = current_texture.get_editor_property("srgb")
+            compression_settings = current_texture.get_editor_property("compression_settings")
+
         importTask:unreal.AssetImportTask = unreal.AssetImportTask()
         importTask.filename = path
+        importTask.async_ = False
         importTask.destination_name = file_name
-        importTask.destination_path = "FOLDER_PATH"
+        importTask.destination_path = folder_path
         importTask.replace_existing = True
         importTask.automated = True
 
         assetTools = unreal.AssetToolsHelpers.get_asset_tools()
         assetTools.import_asset_tasks([importTask])
+
+        if current_texture:
+            current_texture = asset_library.load_asset(folder_path + "/" + file_name)
+            current_texture.set_editor_property("srgb", srgb)
+            current_texture.set_editor_property("compression_settings", compression_settings)
 
 import_textures()
 
