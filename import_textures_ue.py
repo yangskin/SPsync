86449e2bffa346c66d4f4725b6e784ceb551a4f6
yangskin <yangskin@gmail.com>
2024-09-17 22:46:17 +0800

    修正重新导入贴图时参数复用问题

--- import_textures_ue.py
+++ import_textures_ue.py
@@ -10,11 +10,15 @@ def import_textures():
 
     for path in paths:
         file_name = path[path.rfind("/") + 1 :path.rfind(".")]
+        file_path = folder_path + "/" + file_name
 
-        current_texture = asset_library.load_asset(folder_path + "/" + file_name)
-        if current_texture:
-            srgb = current_texture.get_editor_property("srgb")
-            compression_settings = current_texture.get_editor_property("compression_settings")
+        current_texture:unreal.Texture2D = None
+        if asset_library.do_assets_exist([file_path]):
+            current_texture:unreal.Texture2D = asset_library.load_asset(file_path)
+            if current_texture:
+                srgb = current_texture.get_editor_property("srgb")
+                compression_settings = current_texture.get_editor_property("compression_settings")
+                lod_group = current_texture.get_editor_property("lod_group")
 
         importTask:unreal.AssetImportTask = unreal.AssetImportTask()
         importTask.filename = path
@@ -22,15 +26,17 @@ def import_textures():
         importTask.destination_name = file_name
         importTask.destination_path = folder_path
         importTask.replace_existing = True
+        importTask.replace_existing_settings = False
         importTask.automated = True
 
         assetTools = unreal.AssetToolsHelpers.get_asset_tools()
         assetTools.import_asset_tasks([importTask])
 
-        if current_texture:
-            current_texture = asset_library.load_asset(folder_path + "/" + file_name)
+        if current_texture != None:
+            current_texture = asset_library.load_asset(file_path)
             current_texture.set_editor_property("srgb", srgb)
             current_texture.set_editor_property("compression_settings", compression_settings)
+            current_texture.set_editor_property("lod_group", lod_group)
 
 import_textures()
 
